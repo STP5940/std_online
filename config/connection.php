5370d43984ca4b7b10a7c92ff1e33fe71d2b85d7
STP5940 <sitthipong114@gmail.com>
2020-03-12 13:48:18 +0700

    update sql connect

--- config/connection.php
+++ config/connection.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","","std_online") or die("Connect Database error: ".mysqli_error($con));
+$con = mysqli_connect("localhost","root","","sql") or die("Connect Database error: ".mysqli_error($con));
 mysqli_query($con,"SET NAMES 'utf8' ");
 ?>
